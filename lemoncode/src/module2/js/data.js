e0a772315df7f2423a8fa280f8f8554bb1dc4c54
Roberto Valcárcel <rovaldi.web@gmail.com>
2025-01-24 16:54:40 +0100

    Delete lemoncode directory

--- lemoncode/src/module2/js/data.js
+++
@@ -1,8 +0,0 @@
-// Objetivo 1 [Datos del hotel]
-const hotel = {
-	name: 'Mencey',
-	location: 'Tenerife',
-	img: 'https://media-cdn.tripadvisor.com/media/photo-s/16/1a/ea/54/hotel-presidente-4s.jpg',
-};
-
-export default hotel;
